b99d35ac7d7653918d86223c2f62de249119be87
Michał Wesoły <michal.wesoly@silvermedia.pl>
2018-06-19 22:20:55 +0200

    Added backbone for processing orders

--- src/main/java/pl/kozak127/swdramatic/Application.java
+++ src/main/java/pl/kozak127/swdramatic/Application.java
@@ -2,7 +2,9 @@ package pl.kozak127.swdramatic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/pl/kozak127/swdramatic/job/ProcessFieldOrders.java
@@ -0,0 +1,4 @@
+package pl.kozak127.swdramatic.job;
+
+public class ProcessFieldOrders {
+}
